3c4ab2ddd55b2db64a5b4b42c24a5bc85ba741a9
PO3ex2 <poexpoex6@gmail.com>
2025-09-07 16:16:05 +0200

    start -_-

---
+++ src/main/java/net/p3x/svyat_mod/SvyatMod.java
@@ -0,0 +1,17 @@
+package net.p3x.svyat_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SvyatMod implements ModInitializer {
+	public static final String MOD_ID = "svyatmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	//Test Comment
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/p3x/svyat_mod/SvyatModClient.java
@@ -0,0 +1,11 @@
+package net.p3x.svyat_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SvyatModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
