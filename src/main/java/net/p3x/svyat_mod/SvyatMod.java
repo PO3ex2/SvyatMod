1e072e860df673844e7a01fe4a6fdc751845dade
PO3ex2 <poexpoex6@gmail.com>
2025-09-07 16:18:01 +0200

    start -_-

--- src/main/java/net/p3x/svyat_mod/SvyatMod.java
+++ src/main/java/net/p3x/svyat_mod/SvyatMod.java
@@ -9,7 +9,7 @@ public class SvyatMod implements ModInitializer {
 	public static final String MOD_ID = "svyatmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	//Test Comment
+	//ya dolboyeb
 	@Override
 	public void onInitialize() {
 
